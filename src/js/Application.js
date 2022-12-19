be4634c9a8673d0958bc27a8c4d7b86cb0d7f132
petrborr <petrborr@gmail.com>
2022-12-20 01:06:11 +0200

    fix: event listener

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,16 +10,22 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
-
+    this._beat = new Beat()
+    this._beat.on(Beat.events.BIT, () => {
+      if (count < lyrics.length) {
+        this._create(lyrics[count])
+        count ++
+      }
+    })
+    
+    this.emit(Application.events.READY);
+  }
+  _create(word) {
     const message = document.createElement("div");
     message.classList.add("message");
-    message.innerText = "Ah";
-
+    message.innerText = word;
     document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
   }
 }
